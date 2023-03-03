a098adbe97bf6a13cf765afbe3173fbc6474d3a1
Paulo <gapaljohnpaulo@gmail.com>
2023-03-03 08:16:08 +0800

    test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "/index.html",
+    '*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
